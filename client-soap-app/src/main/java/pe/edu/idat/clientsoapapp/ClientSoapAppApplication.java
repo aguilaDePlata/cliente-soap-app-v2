5a4ea19dd05b303d06e0cef03e85298f3556ed58
aguilaDeOro <jorgeluis_illescas@hotmail.com>
2023-06-17 03:29:52 -0500

    feature: SEGUN MODELO

---
+++ client-soap-app/src/main/java/pe/edu/idat/clientsoapapp/ClientSoapAppApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.idat.clientsoapapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientSoapAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClientSoapAppApplication.class, args);
+	}
+
+}
